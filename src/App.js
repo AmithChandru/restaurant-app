19cc8eee8a5aeb6f65d88d840d10ac757d0d520c
AmithChandru <amithc279@gmail.com>
2023-02-26 18:40:21 +0530

    Added description

--- src/App.js
+++ src/App.js
@@ -12,6 +12,11 @@ function App() {
           </div>
         </div>
       </div>
+      <section className='summary'>
+        <h2>Delicious Food, Delivered To You</h2>
+        <p>Choose your favorite meal from our broad sections of available meals and enjoy a delicious lunch or dinner at home.</p>
+        <p>All our meals are cooked with high-quality ingredients, just-in-time and of course by experienced chefs!</p>
+      </section>
     </div>
   );
 }
